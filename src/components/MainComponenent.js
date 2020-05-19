94a8f7bd12f014f5cb383d34683156d44bd5d14e
Hardik Shah <roophardik@gmail.com>
2020-05-19 17:24:43 +0530

    feedback form done

--- src/components/MainComponenent.js
+++ src/components/MainComponenent.js
@@ -9,6 +9,7 @@ import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component{
@@ -48,7 +49,8 @@ class Main extends Component{
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-                    <Route path="/menu/:dishId" component={DishWithId}/>
+                    <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/aboutus" component= {() => <About leaders={this.state.leaders} />} />
                     <Route exact path="/contactus" component={Contact} />} />
                     <Redirect to="/home"/>    
                 </Switch>
